3b438d424851f5558a489b804d370f19caa574be
julianur14 <81245101+julianur14@users.noreply.github.com>
2022-12-27 17:32:06 +0100

    Update q_2

--- q2_test.py
+++ q2_test.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import uniform, gamma, norm
 
+#parte de Simon
 min_value = 1
 max_value = 5
 n = 1
@@ -46,10 +47,14 @@ for i in np.arange(np.size(unif)):
 print(unif)
 print(test)
 
-# Hazte el plot del histograma a ver qué tal queda...
 
 plt.plot(p_val, cdf_vals,'k-')
 # plt.show()
+
+#En teoría hasta aquí genera las muestras de la distribución de p2, pero no termino de entenderlo bien y no parece funcionar pues los valores de la lista se encuentran entre 0 y 1
+
+#Definimos la función de la sigma (más adelante esto podría ir en un archivo tipo utils con funciones auxiliares para varios scripts
+
 def sigma_2_client_decision(alpha_1, alpha_2, N):
     """
     Obtain a sample from the sigma^2 for the client decision
@@ -81,29 +86,8 @@ def prob_client_decision(price_1, price_2, sigma_2):
     # The default Normal distribution is already the standard N(0,1)
     return 1 - norm.pdf((price_1 - price_2) / sigma_2)
 
-def generate_p2(p_val, alpha_1, alpha_2):
-    SAMPLE = [0]
-    M = np.arange(1, 100)
-    v2 = 7
-    value_2         =   5   
-    init_price_2    =   40
-    price_step      =   0.5
-    prices_r2       =   np.arange(value_2, init_price_2, price_step)
-    cdf_vals = np.array([cdf(x) for x in p_val])
-    for h in M:
-        for i in M:
-            p1 = cdf_vals[i]
-        for j in M:
-            sigma = sigma_2_client_decision(cdf_vals[i], alpha_1, alpha_2)
-            MAX = -100
-        for p2 in prices_r2:
-            if sigma != 0:
-                h_p2 = (p2-v2)*np.sum(prob_client_decision(p1, p2, sigma))
-                if h_p2 > MAX:
-                    MAX = h_p2
-                    SAMPLE = np.append(SAMPLE, p2)
-        return SAMPLE
-    
+
+#Aquí estaría la función main, donde los test los meto directamente del código de Simón de la parte superior (por eso está comentado el cálculo)
     
 def generate_p2_2(p_val, alpha_1, alpha_2, N, value_2, init_price, price_step, test):
     SAMPLE = []
@@ -128,4 +112,5 @@ def generate_p2_2(p_val, alpha_1, alpha_2, N, value_2, init_price, price_step, t
             SAMPLE = np.append(SAMPLE, SAMPLE_utility)
     return SAMPLE
 
+#Prueba
 a = generate_p2_2(p_val, 2, 1/2, 100, 7, 40, 0.5, test)        
