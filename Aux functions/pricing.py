deee464ad8fa96d09c86667d876e47770c584b92
julianur14 <81245101+julianur14@users.noreply.github.com>
2023-01-16 12:07:38 +0100

    Update pricing.py

--- Aux functions/pricing.py
+++ Aux functions/pricing.py
@@ -5,15 +5,23 @@ Created on Mon Jan  2 13:02:30 2023
 @author: 42845
 """
 import procons as pc
+import numpy as np
 
 def pricing(p2_samples, prices_r1, alpha_1, alpha_2, p_val, N, value_1):
     MAX = -N
+    list_probs = []
+    util = []
     for price_1 in prices_r1:
         phi = 0
         for price_2 in p2_samples:
+            MAX_2 = -N
             phi = phi + (price_1-value_1)*pc.procons(price_1, price_2, alpha_1, alpha_2, N)
+            if phi > MAX_2:
+                MAX_2 = phi
+        list_probs = np.append(list_probs, pc.procons(price_1, price_2, alpha_1, alpha_2, N))
+        util = np.append(util, (price_1-value_1)*pc.procons(price_1, price_2, alpha_1, alpha_2, N))
         if phi > MAX:
             MAX = phi
             OPT = price_1
-    return OPT
-
+            prob = pc.procons(price_1, price_2, alpha_1, alpha_2, N)
+    return OPT, prob, list_probs, util
